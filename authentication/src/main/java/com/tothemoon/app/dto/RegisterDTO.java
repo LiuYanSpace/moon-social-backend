c8a7e3e0b681fc50cc2161fd6aaadbeda163472b
birdflytomoon <yejia.yang@mycloudpa.com>
2024-02-29 10:55:57 +0000

    update register

--- authentication/src/main/java/com/tothemoon/app/dto/RegisterDTO.java
+++ authentication/src/main/java/com/tothemoon/app/dto/RegisterDTO.java
@@ -1,14 +1,24 @@
 package com.tothemoon.app.dto;
-
 import lombok.Data;
-
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class RegisterDTO {
+    @NotBlank(message = "NickName cannot be blank")
     private String nickName;
+
+    @NotBlank(message = "Username cannot be blank")
     private String username;
+
+    @NotBlank(message = "Friend-of-Friend DoorKey cannot be blank")
     private String fofDoorKey;
+
+    @NotBlank(message = "RecaptchaResponse cannot be blank")
     private String recaptchaResponse;
+
+    @NotBlank(message = "Email cannot be blank")
     private String email;
+
+    @NotBlank(message = "Password cannot be blank")
     private String password;
 }
