de098c1b04775b6974bd811ff227b41956069fec
birdflytomoon <yejia.yang@mycloudpa.com>
2024-03-25 14:24:45 +0000

    update code

--- discussion/src/main/java/com/tothemoon/app/controller/CollectionController.java
+++ discussion/src/main/java/com/tothemoon/app/controller/CollectionController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/collections")
+@RequestMapping("/api/discussions/collections")
 public class CollectionController {
     @Autowired
     private CollectionService collectionService;
